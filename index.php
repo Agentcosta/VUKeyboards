20542e24e08703ba21f8e2172f083ef480ac010b
Agentcosta <mkc.steam.cvb@gmail.com>
2023-05-31 10:31:53 +1200

    Update index.php

--- index.php
+++ index.php
@@ -135,12 +135,7 @@ body {
   </a>
   <div class="desc">Add a description of the image here</div>
 </div>
-    <form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
+    
 
 </body>
 </html>
