33c1789a888dfe0882a17a945bb6f28e31272e10
Agentcosta <mkc.steam.cvb@gmail.com>
2023-06-07 10:59:43 +1200

    wroking on contact form

---
+++ contact.php
@@ -0,0 +1,22 @@
+<html>
+    <h2>Contact Form</h2>
+
+<form action="action_page.php" method="POST"> 
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <label for="email">email:</label><br>
+  <input type="text" id="email" name="email" value="john@mail.com"><br><br>
+ <input type="submit" value="Submit">
+</form>
+<body>
+
+<form action="action.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
