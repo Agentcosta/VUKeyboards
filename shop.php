f178bbdd68e505a02f63395ac409577efe971a4f
Agentcosta <mkc.steam.cvb@gmail.com>
2023-08-03 14:12:53 +1200

    Update shop.php

--- shop.php
+++ shop.php
@@ -6,6 +6,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <style>
+    div.scroll-container {
+  background-color: #333;
+  overflow: auto;
+  white-space: nowrap;
+  padding: 10px;
+}
+
+div.scroll-container img {
+  padding: 10px;
+}
 
 </style>
 </head>
@@ -16,35 +26,47 @@
 <div class="row">
 
     <div class="side">...</div>
-  <div class="main">  <div class="gallery">
+  <div class="main"> 
+      <div class="scroll-container">
+      <div class="gallery">
   <a target="_blank" href="/images/maxresdefault.jpg">
     <img src="/" alt="Keyboard 1" width="600" height="400">
   </a>
   <div class="desc">Add a description of the image here</div>
 </div>
-
+      </div>
+      
+     
+<div class="scroll-container">
 <div class="gallery">
   <a target="_blank" href="img.jpg">
     <img src="/images/mechanical-keyboard-with-rbg.jpg" alt="keyboard 2" width="600" height="400">
   </a>
   <div class="desc">Add a description of the image here</div>
 </div>
+          </div>
 
+          
+<div class="scroll-container">
 <div class="gallery">
   <a target="_blank" href="img_">
     <img src="img_jpg" alt="" width="600" height="400">
   </a>
   <div class="desc">Add a description of the image here</div>
 </div>
-
+</div>
+      <div class="scroll-container">
+          
 <div class="gallery">
   <a target="_blank" href="img_">
     <img src=".jpg" alt="" width="600" height="400">
   </a>
   <div class="desc">Add a description of the image here</div>
-</div>  
-        </div>
 </div>
+          </div>
+     
+</div>
+    </div>
     
   
     <?php include 'footer.php';?>
