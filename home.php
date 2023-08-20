b5592d15f2808c82fdd9733c30d3405dabde70d9
Agentcosta <mkc.steam.cvb@gmail.com>
2023-08-21 08:58:50 +1200

    style sheet home page

--- home.php
+++ home.php
@@ -10,9 +10,10 @@ if (!isset($_SESSION['loggedin'])) {
 <!DOCTYPE html>
 <html>
 	<head>
+        
 		<meta charset="utf-8">
 		<title>Home Page</title>
-		<link href="CSS/login.css" rel="stylesheet" type="text/css">
+		<link href="CSS/style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
